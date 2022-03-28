9796eaf329932baa82fa96dfa80c44b893277219
Dhiraj <Dhiraj@Dhiraj-PC>
2022-03-28 12:34:55 +0530

    added new service class

---
+++ Spring-Boot-REST/src/main/resources/ToDoService.java
@@ -0,0 +1,7 @@
+package com.roytuts.springjspservlet.service;
+import java.util.List;
+import com.roytuts.springjspservlet.model.Todo;
+public interface ToDoService {
+	List<Todo> getToDos();
+	Todo getToDo(String id);
+}
